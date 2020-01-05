da984394fa971554aa84e3e24db40a0541e55297
wojciech.skublewski@wp.pl <wojciech.skublewski@wp.pl>
2020-01-05 16:20:50 +0100

    cd

--- workshops/workshops/main.py
+++ workshops/workshops/main.py
@@ -3,24 +3,26 @@ import argparse
 from clcrypto import check_password
 from database import get_connection, get_cursor
 
-# u = User()
-# u.email = "wojciech.skublewski@wp.pl"
-# u.username = "wojciech.skublewski"
-# u.set_password("Wojtek123")
-#
-# connection = get_connection()
-# cursor = get_cursor(connection)
-#
-# u.save_to_db(cursor)
+u = User()
+u.email = "test@wp.pl"
+u.username = "test"
+u.set_password("Wojtek123")
+
+connection = get_connection()
+cursor = get_cursor(connection)
+
+u.save_to_db(cursor)
+
+connection.close()
+
 
 
 parser = argparse.ArgumentParser(description="Opis naszej aplikacji")
 parser.add_argument('-u', '--user', type=str, help="Pomoc komendy. Użycie -u login")
 parser.add_argument('-p', '--password', type=str, help="Pomoc komendy. Użycie -p hasło")
-parser.add_argument('-a', '--aaa', type=str, help="Pomoc komendy. Użycie -p hasło")
+parser.add_argument('-d', '--delete', type=str, help="Pomoc komendy. Użycie -d usun")
 
 args = parser.parse_args()
-
 
 
 
@@ -32,6 +34,13 @@ if __name__ == '__main__':
         print(f"Podany login: {args.user} a podane hasło to: {args.password}")
         loaded_user = User.load_user_by_username(cursor, args.user)
         if loaded_user and check_password(args.password, loaded_user.hashed_password):
+            print("Poprawne dane")
+        else:
+            print("Błędne dane")
+    if args.user and args.password and args.delete:
+        print(f"Podany login: {args.user} a podane hasło to: {args.password}")
+        if loaded_user and check_password(args.password, loaded_user.hashed_password):
+            loaded_user.delete(cursor)
             print("Poprawne dane")
         else:
             print("Błędne dane")
